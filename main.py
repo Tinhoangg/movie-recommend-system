126a7d55539de851d76c4cd789f06e60a8da8cdd
Tin Hoang <hoangtin050321@gmail.com>
2025-11-20 16:07:15 +0700

    fix conflict

--- main.py
+++ main.py
@@ -40,7 +40,3 @@ def recommend(request: Request, title: str = Query(..., description='Movie title
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  
-    uvicorn.run("main:app", port=port) 
